6b4db347e89122b90b11a1aa7d9871dddf012668
mohamadumarm-toast <125260785+mohamadumarm-toast@users.noreply.github.com>
2023-02-27 11:36:10 +0530

    changed 2 menus to a single menu

--- Task 2 - Payroll Management/TestEmployee.java
+++ Task 2 - Payroll Management/TestEmployee.java
@@ -6,15 +6,15 @@ public class TestEmployee {
         ArrayList<Employee> employees = new ArrayList<>();
         ArrayList<Employee> temp = new ArrayList<>();
         Hashtable<Employee, Integer> dict = new Hashtable<>();
-        menu:
         while(true){
+            MasterData data = new MasterData(employees);
             int  choice = 0;
             boolean flag = false;
-            while(choice < 1 || choice > 2){
+            while(choice < 1 || choice > 6){
                 try{
-                    System.out.println("\nAdd an Employee~\n1.Yes\n2.No\nEnter your choice:");
+                    System.out.println("\n1.Add an Employee\n2.Add Attendance to Employees\n3.Update Attendance by ID\n4.Show Eligible List\n5.Display Employee List\n6.Exit\nEnter your choice:");
                     choice = Integer.parseInt(scanner.nextLine());
-                    if(choice < 1 || choice > 2){
+                    if(choice < 1 || choice > 6){
                         System.out.println(">>>Enter valid choice!");   
                     }
                 }
@@ -41,45 +41,55 @@ public class TestEmployee {
                     catch(NumberFormatException e){
                         System.out.println(">>>Enter valid Salary");
                     }
-                }
+                }    
                 employees.add(employee);
-                temp.add(employee);           
+                temp.add(employee);         
             }
-            else{
-                if(employees.size()!=0){
-                    MasterData data = new MasterData(employees);
-                    System.out.println("\n------------Employees Details-------------");
-                    for (Employee emp : data.getMasterData()) {
-                        // emp.setAllowance();
-                        System.out.println(emp.toString());
-                        System.out.println("----------------------------------------");
-                    }
+            else if(choice == 2){
+                int days;
+                System.out.println("----------------Attendance Portal--------------------");
+                for (Employee employee : temp) {
                     while(true){
-                        choice = 0;
-                        while(choice < 1 || choice > 5){
-                            try{
-                                System.out.println("\n1.Add Attendance to Employees\n2.Update Attendance by ID\n3.Show Eligible List\n4.Go back\n5.Exit\nEnter your choice:");
-                                choice = Integer.parseInt(scanner.nextLine());
-                                if(choice < 1 || choice > 5){
-                                    System.out.println(">>>Enter valid choice!");
-                                }
+                        try{
+                            System.out.println("Id: "+employee.getEmpID());
+                            System.out.println("Name: "+employee.getName());
+                            System.out.println("Enter No. of working days:");
+                            days = Integer.parseInt(scanner.nextLine());
+                            if(days >=  0){
+                                dict.put(employee, days);
+                                break;
                             }
-                            catch(NumberFormatException e){
-                                System.out.println(">>>Enter Single Integer Value!");
+                            else{
+                                System.out.println(">>>Enter non-negative number!");
                             }
                         }
-                        if(choice == 1){
-                            int days;
-                            System.out.println("----------------Attendance Portal--------------------");
-                            for (Employee employee : temp) {
+                        catch(NumberFormatException e){
+                            System.out.println(">>>Enter Integer Value!");
+                        }
+                    }
+                    System.out.println("--------------------------------------------");
+                }
+                temp.clear();
+            }
+            else if(choice == 3){
+                int keyId, days;
+                boolean empFound = false;
+                System.out.println("----------------Update Attendance By ID--------------------");
+                while(true){
+                    try{
+                        System.out.println("Enter Employee ID:");
+                        keyId = Integer.parseInt(scanner.nextLine());
+                        for (Employee emp : employees) {
+                            if(emp.getEmpID() == keyId){
+                                empFound = true;
                                 while(true){
                                     try{
-                                        System.out.println("Id: "+employee.getEmpID());
-                                        System.out.println("Name: "+employee.getName());
+                                        System.out.println("Id: "+emp.getEmpID());
+                                        System.out.println("Name: "+emp.getName());
                                         System.out.println("Enter No. of working days:");
                                         days = Integer.parseInt(scanner.nextLine());
                                         if(days >=  0){
-                                            dict.put(employee, days);
+                                            dict.put(emp, days);
                                             break;
                                         }
                                         else{
@@ -90,81 +100,169 @@ public class TestEmployee {
                                         System.out.println(">>>Enter Integer Value!");
                                     }
                                 }
-                                System.out.println("--------------------------------------------");
-                                flag = true;
+                                break;
                             }
                         }
-                        else if(choice == 2){
-                            int keyId, days;
-                            boolean empFound = false;
-                            System.out.println("----------------Update Attendance By ID--------------------");
-                            while(true){
-                                try{
-                                    System.out.println("Enter Employee ID:");
-                                    keyId = Integer.parseInt(scanner.nextLine());
-                                    for (Employee emp : employees) {
-                                        if(emp.getEmpID() == keyId){
-                                            empFound = true;
-                                            while(true){
-                                                try{
-                                                    System.out.println("Id: "+emp.getEmpID());
-                                                    System.out.println("Name: "+emp.getName());
-                                                    System.out.println("Enter No. of working days:");
-                                                    days = Integer.parseInt(scanner.nextLine());
-                                                    if(days >=  0){
-                                                        dict.put(emp, days);
-                                                        break;
-                                                    }
-                                                    else{
-                                                        System.out.println(">>>Enter non-negative number!");
-                                                    }
-                                                }
-                                                catch(NumberFormatException e){
-                                                    System.out.println(">>>Enter Integer Value!");
-                                                }
-                                            }
-                                            break;
-                                        }
-        
-                                    }
-                                    if(!empFound){
-                                        System.out.println(">>>>Employee not found! Re-enter Valid Employee ID...");
-                                    }
-                                    else
-                                        break;
-                                }
-                                catch(NumberFormatException e){
-                                    System.out.println(">>>Enter Integer Value!");
-                                }
-                            }
+                        if(!empFound){
+                            System.out.println(">>>>Employee not found! Re-enter Valid Employee ID...");
                         }
-                        else if(choice == 3){
-                            if(dict.size()!=0){
-                                AttendanceMaster employeeAttendance = new AttendanceMaster(dict);
-                                System.out.println("-------------Eligible Employees-------------");
-                                employeeAttendance.showEligibleList();
-                            }
-                            else{
-                                System.out.println(">>>>Attendance not yet Entered!");
-                            }
-                        }
-                        else if(choice == 4){
-                            if(flag == true){
-                                temp.clear();
-                            }
-                            continue menu;
-                        }
-                        else{
-                            System.exit(0);
-                        }
+                        else
+                            break;
+                    }
+                    catch(NumberFormatException e){
+                        System.out.println(">>>Enter Integer Value!");
+                    }
+                }
+            }
+            else if(choice == 4){
+                if(dict.size()!=0){
+                    AttendanceMaster employeeAttendance = new AttendanceMaster(dict);
+                    System.out.println("-------------Eligible Employees-------------");
+                    employeeAttendance.showEligibleList();
+                }
+                else{
+                    System.out.println(">>>>Attendance not yet Entered!");
+                }
+            }
+            else if(choice == 5){
+                if(employees.size()!=0){
+                    System.out.println("\n------------Employees Details-------------");
+                    for (Employee emp : data.getMasterData()) {
+                        // emp.setAllowance();
+                        System.out.println(emp.toString());
+                        System.out.println("----------------------------------------");
                     }
                 }
                 else{
                     System.out.println("---No Employee Added!---");
                 }
-                break;
+            }
+            else{
+                System.exit(0);
             }
         }
-        scanner.close();
     }
 }
+//             else{
+//                 if(employees.size()!=0){
+//                     MasterData data = new MasterData(employees);
+//                     System.out.println("\n------------Employees Details-------------");
+//                     for (Employee emp : data.getMasterData()) {
+//                         // emp.setAllowance();
+//                         System.out.println(emp.toString());
+//                         System.out.println("----------------------------------------");
+//                     }
+//                     while(true){
+//                         choice = 0;
+//                         while(choice < 1 || choice > 5){
+//                             try{
+//                                 System.out.println("\n1.Add Attendance to Employees\n2.Update Attendance by ID\n3.Show Eligible List\n4.Go back\n5.Exit\nEnter your choice:");
+//                                 choice = Integer.parseInt(scanner.nextLine());
+//                                 if(choice < 1 || choice > 5){
+//                                     System.out.println(">>>Enter valid choice!");
+//                                 }
+//                             }
+//                             catch(NumberFormatException e){
+//                                 System.out.println(">>>Enter Single Integer Value!");
+//                             }
+//                         }
+//                         if(choice == 1){
+//                             int days;
+//                             System.out.println("----------------Attendance Portal--------------------");
+//                             for (Employee employee : temp) {
+//                                 while(true){
+//                                     try{
+//                                         System.out.println("Id: "+employee.getEmpID());
+//                                         System.out.println("Name: "+employee.getName());
+//                                         System.out.println("Enter No. of working days:");
+//                                         days = Integer.parseInt(scanner.nextLine());
+//                                         if(days >=  0){
+//                                             dict.put(employee, days);
+//                                             break;
+//                                         }
+//                                         else{
+//                                             System.out.println(">>>Enter non-negative number!");
+//                                         }
+//                                     }
+//                                     catch(NumberFormatException e){
+//                                         System.out.println(">>>Enter Integer Value!");
+//                                     }
+//                                 }
+//                                 System.out.println("--------------------------------------------");
+//                                 flag = true;
+//                             }
+//                         }
+//                         else if(choice == 2){
+//                             int keyId, days;
+//                             boolean empFound = false;
+//                             System.out.println("----------------Update Attendance By ID--------------------");
+//                             while(true){
+//                                 try{
+//                                     System.out.println("Enter Employee ID:");
+//                                     keyId = Integer.parseInt(scanner.nextLine());
+//                                     for (Employee emp : employees) {
+//                                         if(emp.getEmpID() == keyId){
+//                                             empFound = true;
+//                                             while(true){
+//                                                 try{
+//                                                     System.out.println("Id: "+emp.getEmpID());
+//                                                     System.out.println("Name: "+emp.getName());
+//                                                     System.out.println("Enter No. of working days:");
+//                                                     days = Integer.parseInt(scanner.nextLine());
+//                                                     if(days >=  0){
+//                                                         dict.put(emp, days);
+//                                                         break;
+//                                                     }
+//                                                     else{
+//                                                         System.out.println(">>>Enter non-negative number!");
+//                                                     }
+//                                                 }
+//                                                 catch(NumberFormatException e){
+//                                                     System.out.println(">>>Enter Integer Value!");
+//                                                 }
+//                                             }
+//                                             break;
+//                                         }
+        
+//                                     }
+//                                     if(!empFound){
+//                                         System.out.println(">>>>Employee not found! Re-enter Valid Employee ID...");
+//                                     }
+//                                     else
+//                                         break;
+//                                 }
+//                                 catch(NumberFormatException e){
+//                                     System.out.println(">>>Enter Integer Value!");
+//                                 }
+//                             }
+//                         }
+//                         else if(choice == 3){
+//                             if(dict.size()!=0){
+//                                 AttendanceMaster employeeAttendance = new AttendanceMaster(dict);
+//                                 System.out.println("-------------Eligible Employees-------------");
+//                                 employeeAttendance.showEligibleList();
+//                             }
+//                             else{
+//                                 System.out.println(">>>>Attendance not yet Entered!");
+//                             }
+//                         }
+//                         else if(choice == 4){
+//                             if(flag == true){
+//                                 temp.clear();
+//                             }
+//                             continue menu;
+//                         }
+//                         else{
+//                             System.exit(0);
+//                         }
+//                     }
+//                 }
+//                 else{
+//                     System.out.println("---No Employee Added!---");
+//                 }
+//                 break;
+//             }
+//         }
+//         scanner.close();
+//     }
+// }
