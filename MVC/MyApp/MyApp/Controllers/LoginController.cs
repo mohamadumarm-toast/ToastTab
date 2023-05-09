e9436fb2b6193046000af7538c7f1990c3e55dc0
mohamadumarm-toast <125260785+mohamadumarm-toast@users.noreply.github.com>
2023-05-09 15:14:16 +0530

    MVC login page

---
+++ MVC/MyApp/MyApp/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+namespace MyApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var key = new User(username, password);
+            var userRepo = new List<User>
+            {
+                new User("john","password123"),
+                new User("mdUmar", "Toast@1234")
+            };
+            if(userRepo.Any(u => u.Username == key.Username && u.Password == key.Password)) { 
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please enter a valid username and password.";
+                Console.WriteLine("Error");
+                return View("Index");
+            }
+            
+        }
+    }
+}
+
---
+++ MVC/MyApp/MyApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApp.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
